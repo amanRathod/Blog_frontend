240ce1caea570abb483de18185f64e09d77a65e9
Aman Rathod <as7677379564@gmail.com>
2021-07-05 23:33:28 +0530

    backened call to blog Posts

--- src/service/backened_call.js
+++ src/service/backened_call.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-empty */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import axios from 'axios';
+import { func } from 'prop-types';
 
 export async function getUserByUserId( followers ) {
     console.log('id', followers)
@@ -13,3 +15,14 @@ export async function getUserByUserId( followers ) {
     }
     
 } 
+
+export async function getAllPosts() {
+    try {
+        console.log()
+        const responseObject  = await axios.get(`http://localhost:4444/getData/allPosts`)
+        console.log(responseObject);
+        return responseObject;
+    } catch (err) {
+        console.log(err);
+    }
+}
